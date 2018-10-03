6df3d532bb514aadb3548d34b6091f82d205693e
Randy Moore <rmoore3891@gmail.com>
2018-10-03 07:08:18 -0500

    Blinky Sketch

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-    pinMode(1,OUTPUT); Serial.begin(9600);
+    pinMode(13,OUTPUT); Serial.begin(9600);
 }
 
 void loop() {
     Serial.println("Hi");
-    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
     delay(1000);
     digitalWrite(13,LOW); delay(1000);
     //Testing GIT Repository
